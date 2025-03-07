92292df408e4d671bed8742d11637b7e092871ba
sing1249 <sing1249@algonquinlive.com>
2025-03-07 18:53:09 -0500

    generate error 3

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -37,4 +37,4 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-}
+
