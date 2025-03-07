e8528b7129af01cddbc7d0d0bb6916a083deaf46
sing1249 <sing1249@algonquinlive.com>
2025-03-07 18:38:36 -0500

    Remove error for test

---
+++ infrastructure/main.tf
@@ -0,0 +1,40 @@
+# Configure the Terraform runtime requirements.
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    # Azure Resource Manager provider and version
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+# Define providers and their config params
+provider "azurerm" {
+    features {}
+}
+
+provider "cloudinit" {
+  # Configuration options
+}
+
+# Resource group "cst8918"
+resource "azurerm_resource_group" "rg" {
+  name = "cst8918"
+  location = "Canada Central"
+}
+
+# Create a new Storage Account in the "cst8918" Resource Group
+resource "azurerm_storage_account" "storage" {
+  name                     = "talwindersinghstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
